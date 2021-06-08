370775ac70bd5f7bc6168e91c5c3d300c6887bcf
Sirisha14 <84261263+Sirisha14@users.noreply.github.com>
2021-06-08 12:07:54 +0530

    Create reverse.cpp

---
+++ reverse.cpp
@@ -0,0 +1,16 @@
+include<bits/stdc++.h>
+using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,res=0,u;
+	cin>>n;
+	while(n!=0)
+	{
+		u=n%10;
+		n=n/10;
+		res=res*10+u;
+	}
+	cout<<endl<<res;
+}
