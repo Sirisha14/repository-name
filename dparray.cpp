ecda188786005792f31f35b6d47dc84f98874ebe
Sirisha14 <84261263+Sirisha14@users.noreply.github.com>
2021-06-08 11:23:15 +0530

    Add files via upload

---
+++ dparray.cpp
@@ -0,0 +1,37 @@
+#include<iostream.h>
+#include<bits/stdc++.h>
+using namespace std;
+int Dp[10000]
+int fact(int n)
+{
+	if(n==0 || n==1)
+	{
+		return 1;
+	}
+	if(Dp[n]=-1)
+	{
+		return DP[n];
+	}
+	return DP[n]=n*fact(n-1)
+}
+int main()
+{
+	int num;
+	cin>>num;
+	for(int i=0;i<=num;i++)
+	{
+		DP[i]=1;
+	}
+	cout<<fact(num);
+}
+
+
+
+
+
+
+	}
+}
+	}
+	}
+}
